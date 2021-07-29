8acd1037212c3a0d2cfef31134ae17d1c0c8c969
“Silas” <songyan_zhao@163.com>
2021-07-29 15:26:45 +0800

    modified

--- Spider.py
+++ Spider.py
@@ -1,36 +1,45 @@
 import requests
 import json
 import time
+import openpyxl
 
 url = "https://report.amap.com/ajax/cityHourly.do?cityCode={}&dataType=4"
 
-#cityCode 可以从excel中找到
-Beijing = "110000"
-Shanghai = "310000"
-Guangzhou="440100"
-Shenzhen="440300"
-Chengdu="510100"
-Hangzhou = "330100"
-Wuhan="420100"
+city_name_list = list(["北京市","上海市","广州市","深圳市","成都市","杭州市","武汉市"])
 
-#所有城市的列表
-cityList = list([["Beijing","110000"],
-                ["Shanghai", "310000"],
-                ["Guangzhou", "440100"],
-                ["Shenzhen", "440300"],
-                ["Chengdu", "510100"],
-                ["Hangzhou" , "330100"],
-                ["Wuhan","420100"]])
+#从excel中找到citycode
+def getCityCode(city,filename):
+    try:
+        file = openpyxl.load_workbook(filename)
+        f = file.active
+    except Exception as e:
+        print(e)
+        exit(1)
+    else:
+        for i in range(1,f.max_row+1): 
+            if f.cell(row=i,column=1).value == city:
+                return f.cell(row=i,column=2).value
 
+#利用city_name_list生成cityList
+def getCityList(city_name_list,filename):
+    cityList = []
+    for i in city_name_list:
+        l = list([i,getCityCode(i,filename)])
+        cityList.append(l)
+    return cityList
+
+#尝试利用cityCode取得该城市的平均时速的列表
 def getList(url,cityCode):
     try:
         response = requests.get(url.format(cityCode))
         response.raise_for_status()
     except requests.RequestException as e:
         print(e)
+        exit(1)
     else:
         return json.loads(response.text)
 
+#从平均时速列表中返回目标时间的平均时速
 #在8-9点时没有数据
 def getNumber(targetTime,responseList):
     for i in responseList:
@@ -43,10 +52,11 @@ def getNumber(targetTime,responseList):
 
 if __name__=='__main__':
     code = []
+    cityList = getCityList(city_name_list,"cityCode.xlsx")
     for i in cityList:
         speed = {i[0]: getNumber(8,getList(url,i[1]))}
         code.append(speed)
-    print(json.dumps(code))
+    print(json.dumps(code,ensure_ascii=False))
 
 
 
